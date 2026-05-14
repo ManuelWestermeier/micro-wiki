c59c54dcf57ce1346d39685808de6615b26537e3
ManuelWestermeier <westermeier111@gmail.com>
2026-05-14 22:24:38 +0200

    render working

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,6 @@
 #include "article-renderer.hpp"
 #include "data.hpp"
 
-Article content = {
-    {H, {"Demokratietheorie & Rechtsstaatsprinzipien"}},
-    {T, {"Demokratie (griech. demos=Volk, kratos=Herrschaft) ist das einzige Regierungssystem, das regelmäßige Machtabloesung ohne Gewalt ermoeglicht."}},
-    {LI, {"Direkte Demokratie: Buerger entscheiden direkt (Schweiz, Athen)", "Repraesentative Demokratie: Vertreter entscheiden (DE, USA)", "Konstitutionelle Monarchie: Monarch eingeschraenkt (GB, Spanien)", "Westminster-System: Regierung vom Parlament abhaengig", "Praesidentielles System: Praesident unabhaengig (USA)", "Verhaeltniswahlrecht: Stimmen → Sitze", "Mehrheitswahlrecht: Gewinner-nimmt-alles", "Foederalismus: Bund vs. Laender"}},
-    {H, {"Internationale Rechtssysteme"}},
-    {LI, {"Common Law: Richterrecht (GB, USA)", "Civil Law: Kodifiziert (DE, FR)", "Scharia: religioeses Recht", "Voelkerrecht: zwischen Staaten", "ICC: Den Haag, Kriegsverbrechen", "WTO: Handelsstreitigkeiten"}}};
-
 // ---------- Setup ----------
 void setup()
 {
@@ -25,5 +18,5 @@ void setup()
 // ---------- Loop ----------
 void loop()
 {
-  renderArticle(content);
+  renderArticle(readArticleBinary("/deutsch/grammatik_und_sprache/sprachsystem.bin"));
 }
