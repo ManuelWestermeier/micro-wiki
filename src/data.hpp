834d26f12f81c3a83b35fbcd7d98adcdcff32e30
ManuelWestermeier <westermeier111@gmail.com>
2026-05-14 22:21:39 +0200

    working

--- src/data.hpp
+++ src/data.hpp
@@ -5,6 +5,8 @@
 #include <vector>
 #include <string>
 
+#include "article-renderer.hpp"
+
 using namespace std;
 
 void initFS()
@@ -28,4 +30,43 @@ vector<string> readDir(const string &path)
     }
 
     return out;
+}
+
+Article readArticleBinary(const std::string &path)
+{
+    Article article;
+
+    File f = LittleFS.open(path.c_str(), "r");
+    if (!f)
+        return article;
+
+    while (f.available())
+    {
+        uint8_t type;
+        uint16_t len;
+
+        if (f.read((uint8_t *)&type, 1) != 1)
+            break;
+        if (f.read((uint8_t *)&len, 2) != 2)
+            break;
+
+        String text = "";
+        text.reserve(len);
+
+        for (int i = 0; i < len; i++)
+        {
+            if (!f.available())
+                break;
+            text += (char)f.read();
+        }
+
+        Item it;
+        it.typ = (Type)type;
+        it.daten.push_back(text);
+
+        article.push_back(it);
+    }
+
+    f.close();
+    return article;
 }
--- wissen/generator.js
+++ wissen/generator.js
@@ -1,15 +1,48 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const wissen = JSON.parse(fs.readFileSync('./wissen.json', 'utf-8'));
+const wissen = JSON.parse(fs.readFileSync("./wissen.json", "utf-8"));
 
-const outDir = "../data"
+const outDir = "../data";
+
+function encodeArticle(articleValue) {
+    let buf = [];
+
+    for (const it of articleValue) {
+        const type = it.typ;
+        const typeId =
+            type === "H" ? 0 :
+                type === "T" ? 1 :
+                    type === "LI" ? 2 : 0;
+
+        const dataArr = Array.isArray(it.daten) ? it.daten : [it.daten];
+
+        for (const d of dataArr) {
+            const text = String(d ?? "");
+            const textBuf = Buffer.from(text, "utf8");
+
+            const b = Buffer.alloc(3);
+            b.writeUInt8(typeId, 0);
+            b.writeUInt16LE(textBuf.length, 1);
+
+            buf.push(b, textBuf);
+        }
+    }
+
+    return Buffer.concat(buf);
+}
 
 for (const [key, value] of Object.entries(wissen)) {
     for (const [subKey, subValue] of Object.entries(value)) {
         for (const [articleKey, articleValue] of Object.entries(subValue)) {
-            const fileName = `${outDir}/${key}/${subKey}/${articleKey}.json`;
-            fs.mkdirSync(`${outDir}/${key}/${subKey}/`, { recursive: true });
-            fs.writeFileSync(fileName, JSON.stringify(articleValue, null, 2));
+
+            const dir = `${outDir}/${key}/${subKey}`;
+            fs.mkdirSync(dir, { recursive: true });
+
+            const fileName = `${dir}/${articleKey}.bin`;
+
+            const bin = encodeArticle(articleValue);
+
+            fs.writeFileSync(fileName, bin);
         }
     }
 }
