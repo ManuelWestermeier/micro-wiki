e92b5bea93003bc2e0b3f52c952750f4c9b802e1
ManuelWestermeier <westermeier111@gmail.com>
2026-05-14 12:28:39 +0200

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -16,26 +16,47 @@ using namespace std;
 
 vector<String> data = {"geschichte", "geographie", "politik_gesellschaft", "sport", "kultur", "wissenschaft_technik", "sonstiges"};
 
-void IRAM_ATTR onButtonInterrupt()
-{
-  digitalWrite(PIN_LED, !digitalRead(PIN_LED));
-}
-
 void setup()
 {
   pinMode(PIN_BUTTON, INPUT_PULLDOWN);
   pinMode(PIN_LED, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(PIN_BUTTON), onButtonInterrupt, RISING);
 
   Wire.begin(PIN_SDA, PIN_SCL);
-
   u8g2.begin();
+}
+
+static int offsetY = 0;
+
+void draw()
+{
   u8g2.clearBuffer();
-  u8g2.setFont(u8g2_font_6x10_tr);
-  u8g2.drawStr(0, 12, "Hello");
+  u8g2.setFont(u8g2_font_5x7_t_cyrillic);
+  for (int i = 0; i < data.size(); i++)
+  {
+    u8g2.drawStr(0, 10 + i * 10 + offsetY, data[i].c_str());
+  }
   u8g2.sendBuffer();
 }
 
 void loop()
 {
+  if (digitalRead(PIN_BUTTON) == LOW)
+  {
+    auto currentTime = millis();
+    bool down = true;
+    while (digitalRead(PIN_BUTTON) == LOW)
+    {
+      if (millis() - currentTime > 500)
+      {
+        offsetY -= 5;
+        down = false;
+        draw();
+      }
+    }
+    if (down)
+    {
+      offsetY += 5;
+      draw();
+    }
+  }
 }
